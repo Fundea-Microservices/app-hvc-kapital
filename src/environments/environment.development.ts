cfb15b47c5e627768c9b7c838691272879502351
iesteban <iesteban@fundea.org.gt>
2026-08-31 12:52:28 -0600

    Ajustes de variables de entorno de  desarrollo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
-const API_URL = 'http://localhost:3000/'
+const API_URL = 'http://localhost:3200/'
 //const API_URL = 'http://172.25.5.11:3030/'
 
 export const environment = {
     production: false,
     path: API_URL,
-    apiPath: (API_URL) + 'v1',
+    apiPath: (API_URL) + 'dev',
     tokenCheckInterval: 5, // minutos
     diasAtrasMovimientos: 1,
 };
